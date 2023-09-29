e0fc1d8ac58e9b778e4df07744533fdb4a2390ac
m-sokolskiy <rognarius@icloud.com>
2023-09-29 11:36:38 +0300

    NavigationBurger (работает)

--- skypro-music/src/components/Navigation.js
+++ skypro-music/src/components/Navigation.js
@@ -1,19 +1,13 @@
 import Logo from './Logo';
 import NavigationBurger from './NavigationBurger';
-import NavigationLink from './NavigationLink';
 import './style/Navigation.css';
-import { useState } from 'react';
 
 // КОМПОНЕНТ НАВИГАЦИИ
-const Navigation = () => {
-    const [visible, setVisible] = useState(false)
-    const toggleVisibility = () => setVisible(!visible);
-    
+const Navigation = () => {  
     return (
         <nav className="main__nav nav">
-            <Logo/>
-            <NavigationBurger onClick={toggleVisibility}/>
-            {visible && (<NavigationLink/>)}
+            <Logo />
+            <NavigationBurger />
         </nav>
     );
 }
--- skypro-music/src/components/NavigationBurger.js
+++ skypro-music/src/components/NavigationBurger.js
@@ -1,13 +1,25 @@
+import NavigationLink from './NavigationLink';
 import './style/NavigationBurger.css'
+import { useState } from 'react';
+
 
 // ВЫПАДАЮЩЕЕ МЕНЮ
 const NavigationBurger = () => {
+
+    const [visible, setVisible] = useState(false)
+
+    const toggleVisibility = () => setVisible(!visible);
+
     return (
-        <div className="nav__burger burger">
-            <span className="burger__line"></span>
-            <span className="burger__line"></span>
-            <span className="burger__line"></span>
+        <div>
+            <div className="nav__burger burger" onClick={toggleVisibility}>
+                <span className="burger__line"></span>
+                <span className="burger__line"></span>
+                <span className="burger__line"></span>
+            </div>
+            {visible && (<NavigationLink />)}
         </div>
+
     );
 }
 export default NavigationBurger

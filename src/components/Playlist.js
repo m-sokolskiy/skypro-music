fe8cbc42e1575800101561631edbac51c50d7ac6
m-sokolskiy <rognarius@icloud.com>
2023-09-30 19:42:01 +0300

    Сделал скелетон в Playlist

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,26 +1,35 @@
 import './style/Playlist.css'
+import 'react-loading-skeleton/dist/skeleton.css'
+import Track from './track';
 
+// МАССИВ ТРЕКОВ
 const playList = {
     tracks: [
         {
-            nickname: "Баста",
+            nickname: "Кинолента",
             img: " ",
-            author: "Nero",
-            album: "Welcome Reality",
+            author: "Баста",
+            album: "Баста 3",
             liked: " ",
-            time: "4:44",
+            time: "4:23",
             id: 1,
         },
+        {
+            nickname: "Ты та...",
+            img: " ",
+            author: "Баста",
+            album: "Баста 1",
+            liked: " ",
+            time: "4:02",
+            id: 2,
+        }
     ]
 }
 
 // ПЛЕЙЛИСТ
 const Playlist = () => {
-
     return (
-
         <div className="centerblock__content">
-
             {/* Титул */}
             <div className="content__title playlist-title">
                 <div className="playlist-title__col col01">Трек</div>
@@ -32,54 +41,12 @@ const Playlist = () => {
                     </svg>
                 </div>
             </div>
-
-            
-                <ul className="content__playlist playlist">
-
-                    {/* Трек */}
-                    {playList.tracks.map((track) => (
-                    <li className="playlist__item" key={track.id}>
-
-                        <div className="playlist__track track">
-
-                            <div className="track__title">
-
-                                <div className="track__title-image">
-
-                                    <svg className="track__title-svg" alt="music">
-                                        <use href="../img/icon/sprite.svg#icon-note"></use>
-                                    </svg>
-
-                                </div>
-
-                                <div className="track__title-text">
-                                    <a className="track__title-link" href="http://">{track.nickname}<span className="track__title-span"></span></a>
-                                </div>
-
-                            </div>
-
-                            <div className="track__author">
-                                <a className="track__author-link" href="http://">{track.author}</a>
-                            </div>
-
-                            <div className="track__album">
-                                <a className="track__album-link" href="http://">{track.album}</a>
-                            </div>
-
-                            <div className="track__time">
-
-                                <svg className="track__time-svg" alt="time">
-                                    <use href="../img/icon/sprite.svg#icon-like"></use>
-                                </svg>
-                                <span className="track__time-text">{track.time}</span>
-
-                            </div>
-                        </div>
-
-                    </li>))}
-
-
-                </ul>
+            <ul className="content__playlist playlist">
+                {/* Трек */}
+                {playList.tracks.map((track) => (
+                    <Track key={track.id} track={track} />
+                ))}
+            </ul>
         </div>
     );
 }

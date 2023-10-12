702b616df2cb52d2b25d567afc1c1bec0e0a41a2
m-sokolskiy <rognarius@icloud.com>
2023-10-12 10:40:21 +0300

    Доработка Выпадающего меню

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,78 +1,46 @@
 import { useState } from 'react';
 import './style/Filter.css'
-import { playListArr } from './Array.js';
+import FilterList from './FilterList';
 
 // ФИЛЬТР
 const Filter = () => {
 
-    const [visibleAuthor, setVisibleAuthor] = useState(false);
-    const [visibleYear, setVisibleYear] = useState(false);
-    const [visibleGenre, setVisibleGenre] = useState(false);
+    const [activeFilter, setActiveFilter] = useState("")
 
-    const PopUpAuthor = () => {
-        setVisibleAuthor(!visibleAuthor);
-        setVisibleYear(false)
-        setVisibleGenre(false)
-    }
+    const activeSlide = (activeClassName) => {
+        if (activeFilter !== activeClassName) {
+            setActiveFilter(activeClassName);
+        } else {
+            setActiveFilter("")
+        }
 
-    const PopUpYear = () => {
-        setVisibleYear(!visibleYear);
-        setVisibleAuthor(false)
-        setVisibleGenre(false)
-    }
-
-    const PopUpGenre = () => {
-        setVisibleGenre(!visibleGenre);
-        setVisibleAuthor(false)
-        setVisibleYear(false)
     }
 
     return (
         <div className="centerblock__filter filter">
 
+            {activeFilter !== "" ? <FilterList name={activeFilter} /> : ""}
             <div className="filter__title">Искать по:</div>
 
             {/* Исполнитель */}
-            <div className="filter__button button-author _btn-text" onClick={() => PopUpAuthor()} >
+            <div className={activeFilter === "button-author" ? "filter__button button-author _btn-text btn-active" : "filter__button button-author _btn-text"} onClick={() => {
+                activeSlide("button-author");
+            }}>
                 исполнителю
             </div>
-            {
-                visibleAuthor && (
-                    <div className="popUp__window-author">
-                        {playListArr.tracks.map((track) => {
-                            return <a className="popUp__link_text" key={track.id}>{track.author}</a>;
-                        })}
-                    </div>
-                )
-            }
-
-
 
             {/* Год выпуска */}
-            <div className="filter__button button-year _btn-text" onClick={() => PopUpYear()}>году выпуска</div>
-            {
-                visibleYear && (
-                    <div className="popUp__window-year">
-                        <a className="popUp__link_text">2020</a>
-                        <a className="popUp__link_text">2021</a>
-                        <a className="popUp__link_text">2022</a>
-                        <a className="popUp__link_text">2023</a>
-                    </div>
-                )
-            }
+            <div className={activeFilter === "button-year" ? "filter__button button-year _btn-text btn-active" : "filter__button button-year _btn-text"} onClick={() => {
+                activeSlide("button-year");
+            }}>году выпуска
+            </div>
 
             {/* Жанр */}
-            <div className="filter__button button-genre _btn-text" onClick={() => PopUpGenre()}>жанру</div>
-            {
-                visibleGenre && (
-                    <div className="popUp__window-genre">
-                        <a className="popUp__link_text">Лирика</a>
-                        <a className="popUp__link_text">Поп</a>
-                        <a className="popUp__link_text">Реп</a>
-                        <a className="popUp__link_text">Хип-Хоп</a>
-                    </div>
-                )
-            }
+            <div className={activeFilter === "button-genre" ? "filter__button button-genre _btn-text btn-active" : "filter__button button-genre _btn-text"} onClick={() => {
+                activeSlide("button-genre");
+            }}>жанру
+            </div>
+
         </div>
     );
 };
---
+++ src/components/FilterList.js
@@ -0,0 +1,37 @@
+import { playListArr } from './Array.js';
+
+// Поп-Ап
+const FilterList = ({ name }) => {
+    let activeClassName = "";
+    if (name === "button-author") {
+        activeClassName = "popUp__window-author";
+        return (
+            <div className={activeClassName}>
+                {playListArr.tracks.map((track) => {
+                    return <a className="popUp__link_text" key={track.id}>{track.author}</a>;
+                })}
+            </div>
+        );
+    } else if (name === "button-year") {
+        activeClassName = "popUp__window-year";
+        return (
+            <div className={activeClassName}>
+                <a className="popUp__link_text">2020</a>
+                <a className="popUp__link_text">2021</a>
+                <a className="popUp__link_text">2022</a>
+                <a className="popUp__link_text">2023</a>
+            </div>
+        )
+    } else if (name === "button-genre") {
+        activeClassName = "popUp__window-genre"
+        return (
+            <div className={activeClassName}>
+                <a className="popUp__link_text">Лирика</a>
+                <a className="popUp__link_text">Поп</a>
+                <a className="popUp__link_text">Реп</a>
+                <a className="popUp__link_text">Хип-Хоп</a>
+            </div>
+        );
+    };
+};
+export default FilterList

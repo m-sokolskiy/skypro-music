5480ea5bddd236ac7cff2ef47b9bb4f0c031d98a
m-sokolskiy <rognarius@icloud.com>
2023-10-22 21:00:21 +0300

    Готова страница регистрации

---
+++ src/pages/registration/registrationPage.jsx
@@ -0,0 +1,29 @@
+import * as S from './style/registrationPage.S'
+
+export const RegistrationPage = () => {
+    return (
+
+        <S.Wrapper>
+            <S.ContainerEnter>
+                <S.ModalBlock>
+                    <S.ModalFormLogin action="#">
+                        <S.ModalFormLoginLink href="#">
+                            <S.ModalLogo>
+                                <S.ModalLogoImg src="../img/logo_modal.png" alt="logo" />
+                            </S.ModalLogo>
+                        </S.ModalFormLoginLink>
+
+                        <S.ModalInputLogin type="text" name="login" placeholder="Почта" />
+                        <S.ModalInputPassword type="password" name="password" placeholder="Пароль" />
+                        <S.ModalInputPasswordDouble class="modal__input password-double" type="password" name="password" placeholder="Повторите пароль"/>
+
+                        <S.ModalBtnSignUpEnt >
+                            <S.ModalBtnSignUpEntLink href="#">Зарегистрироваться</S.ModalBtnSignUpEntLink>
+                        </S.ModalBtnSignUpEnt>
+
+                    </S.ModalFormLogin>
+                </S.ModalBlock>
+            </S.ContainerEnter>
+        </S.Wrapper>
+    );
+};
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import { LoginPage } from "./pages/login/loginPage"
 import { MainPage } from "./pages/main/mainPage"
+import { RegistrationPage } from "./pages/registration/registrationPage"
 
 // Компонент возвращает реестр всех маршрутов
 export const AppRoutes = () => {
     return (
         // Компонент содержит все роуты
         <Routes>
-            <Route path="/" element={<LoginPage/>}/>
-            <Route path="/main" element={<MainPage/>}/>
-            {/* <Route path="/recommendation" element={<LoginPage/>}/>
-            <Route path="/favorites" element={<LoginPage/>}/>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/registration" element={<RegistrationPage />} />
+            {/* <Route path="/favorites" element={<LoginPage/>}/>
+            <Route path="/recommendation" element={<LoginPage/>}/>
             <Route path="/not-found" element={<LoginPage/>}/> */}
         </Routes>
     )

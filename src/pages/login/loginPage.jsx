e34f0dd22821bf166394554f0d045454b1c8402c
m-sokolskiy <rognarius@icloud.com>
2023-10-22 17:41:38 +0300

    работает странца логина но не правиьный стиль

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -1,9 +1,45 @@
-import { Link } from "react-router-dom";
+import * as S from './style/loginPage.S'
 
 export const LoginPage = () => {
     return (
-        <div>
-            <Link to="/main">Войти</Link>
-        </div>
+
+        <S.Wrapper>
+            <S.ContainerEnter>
+
+                <S.ModalBlock>
+
+                    <S.ModalFormLogin action="#">
+
+                        <S.ModalFormLoginLink href="#">
+                            <S.ModalLogo>
+                                <S.ModalLogoImg src="../img/logo_modal.png" alt="logo" />
+                            </S.ModalLogo>
+                        </S.ModalFormLoginLink>
+
+                        <S.ModalInputLogin
+                            type="text"
+                            name="login"
+                            placeholder="Почта"
+                        />
+                        <S.ModalInputPassword
+                            type="password"
+                            name="password"
+                            placeholder="Пароль"
+                        />
+                        <S.ModalBtnEnter >
+                            <S.ModalBtnEnterLink href="#" >Войти</S.ModalBtnEnterLink>
+                        </S.ModalBtnEnter>
+                        
+                        <S.ModalBtnEnterLink >
+                            <S.ModalBtnSignUpLink href="#">Зарегистрироваться</S.ModalBtnSignUpLink>
+                        </S.ModalBtnEnterLink>
+
+                    </S.ModalFormLogin>
+
+                </S.ModalBlock>
+
+            </S.ContainerEnter>
+
+        </S.Wrapper>
     );
 };

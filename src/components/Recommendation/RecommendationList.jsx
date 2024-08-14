02aebc213e5fa6cb6e26ce2d06989144c205d8bf
m-sokolskiy <rognarius@icloud.com>
2024-08-14 12:55:11 +0300

    Проект работает и готов к курсовой

--- src/components/Recommendation/RecommendationList.jsx
+++ src/components/Recommendation/RecommendationList.jsx
@@ -1,18 +1,17 @@
 import { useNavigate } from 'react-router-dom'
-import * as S from './style/RecommendationList.S.js'
 import { useGetSelectionOnIdQuery } from '../../services/trackAPI.js'
+import * as S from './style/RecommendationList.S.js'
 
 const RecommendationList = ({ item }) => {
 
-    const [setSelection, { data, error, isLoading }] = useGetSelectionOnIdQuery
+    // const [setSelection, { data, error, isLoading }] = useGetSelectionOnIdQuery
 
-    const navigation = useNavigate()
+    // const navigation = useNavigate()
 
-
-    const handleSelection = () => {
-        setSelection({ id: data.id, data: data.items});
-        navigation(`/category/${item.id}`)
-    }
+    // const handleSelection = () => {
+    //     setSelection({ id: data.id, data: data.items});
+    //     navigation(`/category/${item.id}`)
+    // }
 
     return (
         <S.SidebarItem >
